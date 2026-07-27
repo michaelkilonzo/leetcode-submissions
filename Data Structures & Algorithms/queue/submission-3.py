5412cc4295b13af196859682504b10a06be0cb2a
michaelkilonzo <68613943+michaelkilonzo@users.noreply.github.com>
2026-07-27 14:55:38 +0000

    Add: queue - submission-3

---
+++ Data Structures & Algorithms/queue/submission-3.py
@@ -0,0 +1,32 @@
+class Deque:
+    
+    def __init__(self):
+        self.queue = []
+
+    def isEmpty(self) -> bool:
+        return len(self.queue) == 0
+
+    def append(self, value: int) -> None:
+        self.queue.append(value)
+
+    def appendleft(self, value: int) -> None:
+        tmp_queue = [] 
+        tmp_queue.append(value)
+
+        for val in self.queue:
+            tmp_queue.append(val)
+
+        self.queue = tmp_queue
+
+
+    def pop(self) -> int:
+        if self.isEmpty():
+            return -1
+        
+        return self.queue.pop() 
+
+    def popleft(self) -> int:
+        if self.isEmpty():
+            return -1
+        
+        return self.queue.pop(0)
