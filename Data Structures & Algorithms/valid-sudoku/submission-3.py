170a6fa651b1bd44d3d7ef5e8feb2ca4aa5c9dc6
michaelkilonzo <68613943+michaelkilonzo@users.noreply.github.com>
2026-08-10 16:25:52 -0400

    Add: valid-sudoku - submission-3

---
+++ Data Structures & Algorithms/valid-sudoku/submission-3.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        rows = [set() for _ in range(9)]
+        cols = [set() for _ in range(9)]
+        subsquares = [set() for _ in range(9)]
+        
+        for i in range(9):
+            for j in range(9):
+                if board[i][j] != ".":
+                    # validate rows and cols and subsquare
+                    if board[i][j] in rows[i] or board[i][j] in cols[j] or board[i][j] in subsquares[((i // 3) * 3 + (j // 3))]:
+                        return False
+                    # add number to row and col sets 
+                    rows[i].add(board[i][j])
+                    cols[j].add(board[i][j])
+                    # add number subsquares set
+                    subsquares[((i // 3) * 3 + (j // 3))].add(board[i][j])
+
+        return True
+
+
